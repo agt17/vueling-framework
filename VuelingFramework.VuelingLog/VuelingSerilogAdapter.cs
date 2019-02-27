60563d98ec36cb3c19c43a90a15cafa9e1eb8ba0
agt17 <alfredgt14@gmail.com>
2019-02-27 13:58:47 +0100

    VuelingSerilogAdapter to use Serilog in the framework

---
+++ VuelingFramework.VuelingLog/VuelingSerilogAdapter.cs
@@ -0,0 +1,61 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VuelingFramework.VuelingLog
+{
+    public class VuelingSerilogAdapter : ITargetAdapterForLogger
+    {
+        private readonly string path = "Logs/VuelingSerilog.txt";
+        public VuelingSerilogAdapter()
+        {
+            Log.Logger = new LoggerConfiguration()
+                        .WriteTo.File(path,
+                            outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+                        .MinimumLevel.Verbose()
+                        .CreateLogger();
+        }
+
+        public TimeSpan ExecutionTime { get; set; }
+        public int counter { get; set; }
+
+        public void Debug(string message)
+        {
+            Log.Debug(message);
+            Log.CloseAndFlush();
+        }
+
+        public void Error(string message)
+        {
+            Log.Error(message);
+            Log.CloseAndFlush();
+        }
+
+        public void Exception(Exception exception, string message)
+        {
+            Log.Error(exception.Message);
+            Log.CloseAndFlush();
+        }
+
+        public void Fatal(string message)
+        {
+            Log.Fatal(message);
+            Log.CloseAndFlush();
+        }
+
+        public void Info(string message)
+        {
+            Log.Information(message);
+            Log.CloseAndFlush();
+        }
+
+        public void Warn(string message)
+        {
+            Log.Warning(message);
+            Log.CloseAndFlush();
+        }
+    }
+}
